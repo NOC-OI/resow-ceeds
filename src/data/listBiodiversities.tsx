a5a380fce7cbb63beba53bad0817e26042b2cb77
tobias ferreira <soutobias@yahoo.com.br>
2023-06-29 09:26:47 +0100

    add corrections to the graph part

--- src/data/listBiodiversities.tsx
+++ src/data/listBiodiversities.tsx
@@ -1,24 +1,6 @@
 /* eslint-disable no-multi-str */
 
 const listValues = [
-  // {
-  //   calcClass: 'Information',
-  //   content: 'This calculation uses XXXXX...',
-  //   calcNames: [
-  //     {
-  //       name: 'Target Sample Size',
-  //       url: 'data?filename=HF2012_other_data,HF2012_annotation_summary&calc=count&crs=epsg%3A4326&extension=csv&column=Habitat',
-  //     },
-  //     {
-  //       name: 'Number of Sample Units',
-  //       url: 'data?filename=HF2012_other_data,HF2012_annotation_summary&calc=unique&crs=epsg%3A4326&extension=csv&column=Habitat',
-  //     },
-  //     {
-  //       name: 'Mean Sample Unit Size',
-  //       url: 'data?filename=HF2012_other_data,HF2012_annotation_summary&calc=unique&crs=epsg%3A4326&extension=csv&column=Habitat',
-  //     },
-  //   ],
-  // },
   {
     calcClass: 'Diversity across survey',
     content:
--- src/data/listIndicatorSpecies.tsx
+++ src/data/listIndicatorSpecies.tsx
@@ -1,31 +1,4 @@
 /* eslint-disable no-multi-str */
-// const organisms = [
-//   'Pentapora foliacea',
-//   'Cartilagenous fish',
-//   'Axinellidae_spp',
-//   'Porella sp.',
-//   'Parazoanthus sp.',
-//   'Anthozoa_34',
-//   'Anthozoa_39',
-//   'Lepidorhombus whiffiagonis',
-//   'Perciformes spp.10',
-//   'Gadidae spp.',
-//   'Paguridae_02',
-//   'Bolocera spp.',
-//   'Anthozoa_16',
-//   'Cerianthid_01',
-//   'Anthozoa_03',
-//   'Porifera_20',
-//   'Salamancina dysteri',
-//   'Munida sp.',
-//   'Echinus esculentus',
-//   'Reteporella spp.',
-//   'Stichastrrella rosea',
-//   'Antedon spp.',
-//   'Paguridae_01',
-//   'Liocarcinus spp.',
-//   'Hippoglossoides platessoides',
-// ]
 
 interface keyable {
   [key: string]: any

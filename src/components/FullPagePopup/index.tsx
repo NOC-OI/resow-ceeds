c93e02359f47b9d7bfd13e1a048ccef860654827
tobfer <soutobias@yahoo.com.br>
2023-12-05 17:41:04 +0000

    add logo

--- src/components/FullPagePopup/index.tsx
+++ src/components/FullPagePopup/index.tsx
@@ -1,4 +1,8 @@
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import {
+  faCircleXmark,
+  faCode,
+  faVideo,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { FullPagePopupContainer } from './styles'
 
@@ -16,16 +20,31 @@ export function FullPagePopup({ setShowPopup }: FullPagePopupProps) {
       <div className="w-[40rem] align-middle text-center">
         <FontAwesomeIcon icon={faCircleXmark} onClick={handleClose} />
         <div>
-          <img src="favicon_CEEDS.png" className="h-40" />
+          <img src="logo.png" className="h-28" />
         </div>
-        <h2 className="text-center font-bold pb-3 capitalize text-5xl">
-          CEEDS
+        <h2 className="text-center font-bold pb-3 capitalize text-4xl">
+          CEEDs Tool
         </h2>
         <div className="p-4">
-          <p className="text-center font-bold text-3xl">
-            An AI-powered model to guide nature-based solutions to climate
-            change
+          <p className="text-center font-bold text-2xl">
+            Some text about the tool
           </p>
+        </div>
+        <div className="p-4">
+          <div className="grid grid-cols-2 gap-1">
+            <a href="" target="_blank" className="p-4 cursor-pointer">
+              <FontAwesomeIcon icon={faVideo} />
+              <p className="text-center text-sm font-bold">About</p>
+            </a>
+            <a
+              href="https://imfe-pilot-documentation.co.uk/"
+              target="_blank"
+              className="p-4 cursor-pointer"
+            >
+              <FontAwesomeIcon icon={faCode} />
+              <p className="text-center text-sm font-bold">Documentation</p>
+            </a>
+          </div>
         </div>
       </div>
     </FullPagePopupContainer>
